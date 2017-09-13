95a4576d9338b7091555d1db96f3e4c064fdaad6
Edwin Gamboa <edwjov92@gmail.com>
2017-09-13 10:12:46 -0500

    CUBBLES-80: Code improvement

--- js/background.js
+++ js/background.js
@@ -24,9 +24,6 @@
       }
     });
   });
-
-  // Receive message from content script and relay to the devTools page for the
-  // current tab
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     // Messages from content scripts should have sender.tab set
     if (sender.tab) {
